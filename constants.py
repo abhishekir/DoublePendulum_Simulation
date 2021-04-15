990fb1d5084f3752dc60eacca6df5e9e715e5783
abhishekir <abhishek.ir3@gmail.com>
2021-04-15 12:04:28 -0700

    manipulating constants to find crashing bug

--- constants.py
+++ constants.py
@@ -1,12 +1,40 @@
 import pygame
+import math
 
 # constants
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-GAME_FPS = 60
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+GAME_FPS = 30
 GAME_CLOCK = pygame.time.Clock()
 
 GAME_CAPTION = "Double Pendulum Sim"
 GAME_ICON = "media/game_icon.png"
-BACKGROUND_COLOR = (0, 0, 50)
+# BACKGROUND_COLOR = (0, 0, 50)
 
+white = (255, 255 , 255)
+black = (0, 0, 0)
+
+mass1 = 80
+mass2 = 40
+length1 = 150
+length2 = 150
+
+angle1 = math.pi/2
+angle2 = math.pi/2
+angle_velocity1 = 0
+angle_velocity2 = 0
+angle_acceleration1 = 0
+angle_acceleration2 = 0
+
+Gravity = 9.8
+scatter1 = []
+scatter2 = []
+
+starting_point = (int(SCREEN_WIDTH/2) , int(SCREEN_HEIGHT/4))
+
+x_offset = starting_point[0]
+y_offset = starting_point[1]
+
+import formula
+import point
+import random
--- main.py
+++ main.py
@@ -11,12 +11,50 @@ def initialize():
 
     return screen
 
+
+def logic(screen):
+    global angle1, angle2, angle_velocity1, angle_velocity2
+    # calculate the acceleration
+    angle_acceleration1 = formula.FirstAcceleration(angle1, angle2, mass1, mass2, length1, length2, Gravity,
+                                                     angle_velocity1, angle_velocity2)
+    angle_acceleration2 = formula.SecondAcceleration(angle1, angle2, mass1, mass2, length1, length2, Gravity,
+                                                      angle_velocity1, angle_velocity2)
+
+    x1 = float(length1 * math.sin(angle1) + x_offset)
+    y1 = float(length1 * math.cos(angle1) + y_offset)
+
+    x2 = float(x1 + length2 * math.sin(angle2))
+    y2 = float(y1 + length2 * math.cos(angle2))
+
+    # the angle varies with respect to the velocity and the velocity with respect to the acceleration
+    angle_velocity1 += angle_acceleration1
+    angle_velocity2 += angle_acceleration2
+    angle1 += angle_velocity1
+    angle2 += angle_velocity2
+
+    scatter1.append((x1, y1))
+    scatter2.append((x2, y2))
+
+    for my_point in scatter2:
+        # random_color = (random.randint(20, 255), random.randint(20, 255), random.randint(20, 255))
+        plot = point.Point(my_point[0], my_point[1], screen, white, scatter2)
+        plot.draw()
+
+    pygame.draw.line(screen, white, starting_point, (x1, y1), 6)
+
+    pygame.draw.line(screen, white, (x1, y1), (x2, y2), 6)
+    pygame.draw.circle(screen, white, (int(x2), int(y2)), 10)
+    pygame.draw.circle(screen, (20, 200, 30), (int(x1), int(y1)), 20)
+    if len(scatter1) > 1:
+        pygame.draw.lines(screen, (100, 50, 100), False, scatter1, 1)
+
+
 def main(screen):
     running = True
 
     while running:
         GAME_CLOCK.tick(GAME_FPS)
-        screen.fill(BACKGROUND_COLOR)
+        screen.fill(black)
 
         # Key Handler
         for event in pygame.event.get():
@@ -29,6 +67,8 @@ def main(screen):
         # Game in-progress
         if running:
             # Logic Loop Handler
+            logic(screen)
+
             pygame.display.update()
         else:
             pygame.quit()
